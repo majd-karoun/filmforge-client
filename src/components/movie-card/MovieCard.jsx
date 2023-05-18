08412a588d4cb2045c61e733f7e3da4f524d2896
Majd Karoun <107414000+majd-karoun@users.noreply.github.com>
2023-05-18 13:10:28 +0200

    added proptypes

--- src/components/movie-card/MovieCard.jsx
+++ src/components/movie-card/MovieCard.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -7,4 +9,16 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       {movie.title}
     </div>
   );
+};
+
+MovieCard.PropTypes = {
+  movie: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    genre: PropTypes.string,
+    director: PropTypes.string,
+    description: PropTypes.string,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };
